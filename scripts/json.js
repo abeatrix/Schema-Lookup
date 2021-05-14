09a47a4864329e946ed2ed85c2af933cc42fc3c3
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-05-13 23:09:05 -0700

    Add style and title

--- scripts/json.js
+++ scripts/json.js
@@ -6,8 +6,8 @@ $(document).ready(()=>{
 
         $.each(data.properties, (k, v) => {
             const d = v.description, t = v.type, g = v.group;
-            row += d.includes("DEPRECATED") ? "<tr class='removed'>" :"<tr>";
-            row += `<td>${key}</td><td>${d}</td><td>${t}</td><td>${g}</td></tr>`;
+            row += d.includes("DEPRECATED") ? "<tr class='removed p-2'>" :"<tr class='p-2'>";
+            row += `<td>${k}</td><td>${d}</td><td>${t}</td><td>${g}</td></tr>`;
         })
 
         $("#site_config_1").append(row);
