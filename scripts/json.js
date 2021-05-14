fcc8e0d46ffaf21cc9e76e8d4d38518da7718d0e
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-05-14 00:30:33 -0700

    Update table

--- scripts/json.js
+++ scripts/json.js
@@ -10,14 +10,20 @@ $(document).ready(()=>{
             row += `<td>${k}</td><td>${d}</td><td>${t}</td><td class="group">${g}</td></tr>`;
         })
 
+        $("#site-config-1").append(row);
+
+        row = "";
+
         $.each(data.definitions, (k, v) => {
             const d = v.description, t = v.type;
             row += d.includes("DEPRECATED") || d.includes("removed") ? "<tr class='removed p-2'>" :"<tr class='p-2'>";
             row += `<td>${k}</td><td>${d}</td><td>${t}</td><td class="group">Definitions</td></tr>`;
         })
 
-        $("#site-config-1").append(row);
+        $("#site-config-2").append(row);
 
     })
 
 })
+
+
