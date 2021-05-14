85cbb00d249379772dfbd4800ad99b19a480941a
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-05-13 22:50:04 -0700

    basic render complete

---
+++ scripts/json.js
@@ -0,0 +1,16 @@
+$(document).ready(()=>{
+
+    $.getJSON("https://raw.githubusercontent.com/sourcegraph/sourcegraph/main/schema/site.schema.json", (data) => {
+
+        let row = "";
+
+        $.each(data.properties, (k, v) => {
+            const d = v.description, t = v.type, g = v.group;
+            row += d.includes("DEPRECATED") ? "<tr class='removed'>" :"<tr>";
+            row += `<td>${key}</td><td>${d}</td><td>${t}</td><td>${g}</td></tr>`;
+        })
+
+        $("#site_config_1").append(row);
+
+    })
+})
